fef80bedb3535ecee68f526ab5e684d6d5471008
manami young <manayou1125@outlook.jp>
2022-07-07 15:34:46 +0900

    課題の提出

---
+++ sample_pg.rb
@@ -0,0 +1,29 @@
+# pgライブラリを使用する
+require 'pg'
+
+# PG::connect(dbname: "goya")により、rubyからgoyaDBに接続し
+# 接続したという情報をconnectionという名前の変数に格納する
+connection = PG::connect(dbname: "goya")
+connection.internal_encoding = "UTF-8"
+begin
+  # connection変数を使いPostgreSQLを操作する
+  # .execで、goyaDBに"select weight, give_for from crops;"
+  # のSQLの命令文を直接実行し、その結果をresult変数に格納する
+  result = connection.exec("select * from crops where not (give_for = '自家消費');")
+
+  # 取り出した各行を処理する
+  result.each do |record|
+      # 各行を取り出し、putsでターミナル上に出力する
+      puts "譲渡先が自家消費ではないもの：#{record["give_for"]}"
+
+  result = connection.exec("select quality from crops where quality = false;")
+
+  # 取り出した各行を処理する
+  result.each do |record|
+      # 各行を取り出し、putsでターミナル上に出力する
+      puts "品質が悪いもの：#{record["quality"]}"
+  end
+ensure
+  # 最後に.finishでデータベースへのコネクションを切断する
+  connection.finish
+end
